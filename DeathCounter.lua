b40b926670fa011319783e393a2422db51f18542
XeroState <37225923+XeroState@users.noreply.github.com>
2022-09-20 17:18:14 -0600

    v1

--- DeathCounter.lua
+++ DeathCounter.lua
@@ -5,6 +5,7 @@ f:SetScript("OnEvent", function(self, event)
     self:COMBAT_LOG_EVENT_UNFILTERED(CombatLogGetCurrentEventInfo())
 end)
 
+if DeathPlyaer == nil then DeathPlayer = "Ootan" end
 if DeathCount == nil then DeathCount = 0 end
 
 function f:COMBAT_LOG_EVENT_UNFILTERED(...)
@@ -16,13 +17,31 @@ function f:COMBAT_LOG_EVENT_UNFILTERED(...)
     end
 end
 
-
-local function ANNOUNCE(type)
-    if type == "s" then SendChatMessage(("I've witnessed Ootan die %s times now"):format(DeathCount), "SAY") end
-    if type == "p" then SendChatMessage(("I've witnessed Ootan die %s times now"):format(DeathCount), "PARTY") end
-    if type == "r" then SendChatMessage(("I've witnessed Ootan die %s times now"):format(DeathCount), "RAID") end
-    if type == "rw" then SendChatMessage(("I've witnessed Ootan die %s times now"):format(DeathCount), "RAID_WARNING") end
+SlashCmdList["DC"] = function(msg)
+    if msg == "s" then
+        SendChatMessage(("I've witnessed Freesia die %s times now"):format(DeathCount), "SAY")
+    elseif msg == "p" then
+        SendChatMessage(("I've witnessed Freesia die %s times now"):format(DeathCount), "PARTY")
+    elseif msg == "r" then
+        SendChatMessage(("I've witnessed Freesia die %s times now"):format(DeathCount), "RAID")
+    elseif msg == "rw" then
+        SendChatMessage(("I've witnessed Freesia die %s times now"):format(DeathCount), "RAID_WARNING")
+    elseif msg == "reset" then
+        DeathCount = 0
+    elseif msg == "count" then
+        print(("Freesia Death Count: %s"):format(DeathCount))
+    elseif msg == "track" then
+        DeathPlayer = "%s"
+    elseif msg == "" or msg == "h" or msg == "help" or msg == "?" then
+        print("\nXEROSTATE'S CUSTOM DEATH COUNTER")
+        print("\n\n/dc track PLAYER -> Configures which player to death track, also resets count to 0")
+        print("\n/dc reset -> Resets the DeathCount to 0")
+        print("\n\n/dc s -> Announces DeathCount to SAY")
+        print("\n/dc p -> Announces DeathCount to PARTY")
+        print("\n/dc r -> Announces DeathCount to RAID")
+        print("\n/dc rw -> announces DeathCount to RAID WARNING")
+        print("\n/dc count -> Prints current death count to console/n ")
+    end
 end
-
-SLASH_DC1 = '/dc'
-SlashCmdList["DC"] = ANNOUNCE
+SLASH_DC1 = "/dc";
+SLASH_DC2 = "/deathcount";
