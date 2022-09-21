5b3f342eec21d1376329cef5b98c7f2fa4e2409d
XeroState <37225923+XeroState@users.noreply.github.com>
2022-09-20 19:40:33 -0600

    v5

--- DeathCounter.lua
+++ DeathCounter.lua
@@ -1,45 +1,74 @@
+-- CODE BITCHES!
 
+
+-- From what I can tell, this just tracks the Combat Event Log
 local f = CreateFrame("Frame")
 f:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 f:SetScript("OnEvent", function(self, event)
     self:COMBAT_LOG_EVENT_UNFILTERED(CombatLogGetCurrentEventInfo())
 end)
 
-if DeathPlyaer == nil then DeathPlayer = "Ootan" end
+-- Sets variables if they are not already set
+if DeathPlayer == nil then DeathPlayer = "Freesia" end
 if DeathCount == nil then DeathCount = 0 end
+if FreesiaDeathCount == nil then FreesiaDeathCount = 0 end
 
+-- Looks at the event log
+-- If it's someone dying, it checks to see if they are the player tracked or Freesia and updates variable accordingly
 function f:COMBAT_LOG_EVENT_UNFILTERED(...)
     local timestamp, subevent, _, sourceGUID, sourceName, sourceFlags, sourceRaidFlags, destGUID, destName, destFlags, destRaidFlags = ...
     if subevent == "UNIT_DIED" then
-        if destName=="Ootan" then 
+        if destName==DeathPlayer then 
             DeathCount = DeathCount+1
+        end
+        if destName=="Freesia" then
+            FreesiaDeathCount = FreesiaDeathCount+1
         end
     end
 end
 
+-- Slash Command List
 SlashCmdList["DC"] = function(msg)
-    if msg == "s" then
+    -- If we're to track Freesia, change the DeathCount to the global FreesiaDeathCount value
+    if DeathPlayer == "Freesia" then DeathCount = FreesiaDeathCount end
+
+    -- Say Channel
+    if msg == "s" or msg == "say" then
         SendChatMessage(("I've witnessed %s died %s times now"):format(DeathPlayer, DeathCount), "SAY")
-    elseif msg == "p" then
+    -- Party Channel
+    elseif msg == "p" or msg == "party" then
         SendChatMessage(("I've witnessed %s died %s times now"):format(DeathPlayer, DeathCount), "PARTY")
-    elseif msg == "r" then
+    -- Raid Channel
+    elseif msg == "r" or msg == "raid" then
         SendChatMessage(("I've witnessed %s died %s times now"):format(DeathPlayer, DeathCount), "RAID")
-    elseif msg == "rw" then
+    -- Raid Warning Channel
+    elseif msg == "rw" or msg == "warning" then
         SendChatMessage(("I've witnessed %s died %s times now"):format(DeathPlayer, DeathCount), "RAID_WARNING")
+    -- Reset counter to 0, does NOT reset Freesia global counter
     elseif msg == "reset" then
         DeathCount = 0
+    -- Prints current amount
     elseif msg == "count" then
-        print(m2:format(DeathCount))
+        print(("%s Death Count: %s"):format(DeathPlayer, DeathCount))
+    -- Changes who you are tracking via your current target
+    elseif msg == "track" then
+        DeathPlayer = select(6, GetPlayerInfoByGUID(UnitGUID("target")))
+        DeathCount = 0
+    -- Help Menu
     elseif msg == "" or msg == "h" or msg == "help" or msg == "?" then
-        print("\nXEROSTATE'S CUSTOM DEATH COUNTER")
-        print("\n\n/dc track PLAYER -> Configures which player to death track, also resets count to 0")
-        print("\n/dc reset -> Resets the DeathCount to 0")
-        print("\n\n/dc s -> Announces DeathCount to SAY")
-        print("\n/dc p -> Announces DeathCount to PARTY")
-        print("\n/dc r -> Announces DeathCount to RAID")
-        print("\n/dc rw -> announces DeathCount to RAID WARNING")
-        print("\n/dc count -> Prints current death count to console/n ")
+        print("\n|cfffe00f1XeroState's Custom Death Counter|r")
+        print("\n")
+        print("|cffe0d016/dc track|r -> Tracks the player currently targetted, also resets count to 0")
+        print("       |cff32a852Freesia's death count will always be tracked in the background|r")
+        print("|cffe0d016/dc reset|r -> Resets the DeathCount to 0")
+        print("       |cff32a852Freesia's death count will not be reset|r")
+        print("|cffe0d016/dc s|r -> Announces DeathCount to SAY")
+        print("|cffe0d016/dc p|r -> Announces DeathCount to PARTY")
+        print("|cffe0d016/dc r|r -> Announces DeathCount to RAID")
+        print("|cffe0d016/dc rw|r -> announces DeathCount to RAID WARNING")
+        print("|cffe0d016/dc count|r -> Prints current death count to console")
     end
 end
+-- Valid Slash Commands
 SLASH_DC1 = "/dc";
 SLASH_DC2 = "/deathcount";
